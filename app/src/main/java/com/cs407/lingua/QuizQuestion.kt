fab1e1927a3a3eb8af0155f32c9e875331493742
Vincent Chen <vchen7142@gmail.com>
2024-11-16 18:43:41 -0600

    Added things to the Exercise Selection Fragment

--- app/src/main/java/com/cs407/lingua/QuizQuestion.kt
+++ app/src/main/java/com/cs407/lingua/QuizQuestion.kt
@@ -5,7 +5,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.navigation.fragment.findNavController
@@ -17,14 +16,19 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [ExerciseSelection.newInstance] factory method to
+ * Use the [QuizQuestion.newInstance] factory method to
  * create an instance of this fragment.
  */
-class ExerciseSelection : Fragment() {
+class QuizQuestion : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
         }
     }
 
@@ -33,17 +37,7 @@ class ExerciseSelection : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val view = inflater.inflate(R.layout.fragment_exercise_selection, container, false)
-        val quizTitle = view.findViewById<TextView>(R.id.quizTitle)
-        val args = this.arguments
-        when(args?.getInt("exerciseType")) {
-            1 -> {
-                quizTitle.text = "Simple Syntax"
-            }
-            2 -> quizTitle.text = "Compound Syntax"
-            3 -> quizTitle.text = "Complex Syntax"
-        }
-        return view
+        return inflater.inflate(R.layout.fragment_quiz_question, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -70,12 +64,12 @@ class ExerciseSelection : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment ExerciseSelection.
+         * @return A new instance of fragment QuizQuestion.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            ExerciseSelection().apply {
+            QuizQuestion().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
