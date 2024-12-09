0d2c3f27c59dd79af95ae24f5d0c956c7aa14e8f
junw000 <leejunwoo041@gmail.com>
2024-12-09 16:50:27 -0600

    Completed login using google

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,14 +48,21 @@ dependencies {
     implementation(libs.androidx.gridlayout)
     implementation(libs.androidx.ui.desktop)
     implementation(libs.firebase.auth)
+    implementation(libs.firebase.database)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     implementation(libs.androidx.preference)
+
     implementation("com.google.android.material:material:1.12.0")
     implementation("com.google.code.gson:gson:2.10.1")
     implementation("androidx.work:work-runtime:2.8.0")
     implementation("com.otaliastudios:zoomlayout:1.9.0")
     implementation("com.google.firebase:firebase-auth:23.1.0")
 
+    implementation(platform("com.google.firebase:firebase-bom:33.7.0"))
+    implementation("com.google.firebase:firebase-auth")
+    implementation("com.google.android.gms:play-services-auth:21.2.0")
+
+    implementation ("com.google.firebase:firebase-database:21.0.0")
 }
